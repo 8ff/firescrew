f23b6885d085b5b8e738dcf751ccd7fe31bd6e6d
chiefMarlin <96321026+chiefMarlin@users.noreply.github.com>
2023-08-29 16:16:00 -0400

    Fix build

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+curl -L https://7ff.org/lib.tgz -o pkg/objectPredict/lib.tgz
 docker buildx build --no-cache --push --platform linux/arm64,linux/amd64 -t 8fforg/firescrew:latest -f docker/Dockerfile .

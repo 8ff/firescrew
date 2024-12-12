7bf9be4eebdea4bec0251d78cb9d0d9257f653c2
chiefMarlin <96321026+chiefMarlin@users.noreply.github.com>
2024-12-12 10:33:28 -0500

    Adjust demo timing

--- docker/run-firescrew.sh
+++ docker/run-firescrew.sh
@@ -52,7 +52,7 @@ if [ "$#" -eq 1 ] && [ "$1" = "demo" ]; then
   -c:v libx264 -preset veryfast -tune zerolatency -g 5 -r 25 -s 1920x1080 -b:v 5000k -rtsp_transport tcp -f rtsp rtsp://localhost:8554/hi >/pid3.log 2>&1|| fail "Failed to start ffmpeg" &
   PID3=$!
 
-  sleep 1
+  sleep 5
 
   echo "[+] [4/5] Starting motion detection..."
   nohup "$BINARY_PATH" ./demoConfig.json >/pid4.log 2>&1|| fail "Failed to start firescrew" &
